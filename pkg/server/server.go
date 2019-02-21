9d78ba4447e2eba34cdb9358e0be932426936b90
Levon <crackon0410@gmail.com>
2019-02-21 18:39:14 +0300

    Add middleware, add logger

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,6 +1,10 @@
 package server
 
 import (
+	"log"
+	"net/http"
+	"time"
+
 	"github.com/Lighty0410/ekadashi-server/pkg/mongo"
 	"github.com/gorilla/mux"
 )
@@ -17,8 +21,36 @@ func NewEkadashiServer(db *mongo.Service) (*EkadashiServer, error) {
 		Router: mux.NewRouter(),
 		db:     db,
 	}
-	s.Methods("POST").Path("/register").HandlerFunc(s.handleRegistration)
-	s.Methods("POST").Path("/login").HandlerFunc(s.handleLogin)
-	s.Methods("GET").Path("/users").HandlerFunc(s.showAllUsers)
+	s.Router.Handle("/register", chainMiddleware(http.HandlerFunc(s.handleRegistration), withLogging, withTracing)).
+		Methods("POST")
+	s.Router.Handle("/login", chainMiddleware(http.HandlerFunc(s.handleLogin), withLogging, withTracing)).
+		Methods("POST")
+	s.Router.Handle("/users", chainMiddleware(http.HandlerFunc(s.showAllUsers), withTracing, withLogging)).
+		Methods("GET")
 	return s, nil
+}
+
+type middleware func(http.Handler) http.Handler
+
+func chainMiddleware(handler http.Handler, mw ...middleware) http.Handler {
+	for _, m := range mw {
+		handler = m(handler)
+	}
+	return handler
+}
+
+func withLogging(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Logged connection from %s", r.ParseForm())
+		next.ServeHTTP(w, r)
+	})
+}
+
+func withTracing(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		next.ServeHTTP(w, r)
+		log.Println(time.Since(start))
+		log.Println(r.Method)
+	})
 }
