e97722c603e74ad78c6b2362a791bf6358e392fc
Levon <crackon0410@gmail.com>
2019-03-11 16:30:23 +0300

    Refactor handlers tests

--- pkg/server/handlers_test.go
+++ pkg/server/handlers_test.go
@@ -2,14 +2,15 @@ package server
 
 import (
 	"bytes"
-	"encoding/json"
-	"github.com/Lighty0410/ekadashi-server/pkg/mongo"
-	"gotest.tools/assert"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httptest"
 	"os"
 	"testing"
+
+	"github.com/Lighty0410/ekadashi-server/pkg/mongo"
+	"github.com/stretchr/testify/assert"
 )
 
 type message struct {
@@ -60,93 +61,113 @@ func TestRegisterFunc(t *testing.T) {
 			request: `{"username":"1234u sername",
               "password":"12414password"}`,
 			expectedResponse: http.StatusBadRequest,
-			expectedMessage:  "field username contain latin characters and numbers without space only",
+			expectedMessage:  "{\"reason\":\"field username contain latin characters and numbers without space only\"}\n",
 		},
 		{
 			name: "status 400 with space inside field password",
 			request: `{"username":"1234username",
               "password":"12414pa  ssword"}`,
 			expectedResponse: http.StatusBadRequest,
-			expectedMessage:  "field password contain latin characters and numbers without space only",
+			expectedMessage:  "{\"reason\":\"field password contain latin characters and numbers without space only\"}\n",
 		},
 		{
 			name: "status 400 with ASCII symbols inside field password",
 			request: `{"username":"1234username",
               "password":"12414pa  ssword"}`,
 			expectedResponse: http.StatusBadRequest,
-			expectedMessage:  "field password contain latin characters and numbers without space only",
+			expectedMessage:  "{\"reason\":\"field password contain latin characters and numbers without space only\"}\n",
 		},
 		{
 			name: "status 400 with ASCII symbols inside field username",
 			request: `{"username":"123user@&!*",
               "password":"12414password"}`,
 			expectedResponse: http.StatusBadRequest,
-			expectedMessage:  "field username contain latin characters and numbers without space only",
+			expectedMessage:  "{\"reason\":\"field username contain latin characters and numbers without space only\"}\n",
 		},
 		{
 			name: "status 400 with ASCII symbols inside field password",
 			request: `{"username":"iamuserhah",
               "password":"123pas@(&!(s"}`,
 			expectedResponse: http.StatusBadRequest,
-			expectedMessage:  "field password contain latin characters and numbers without space only",
+			expectedMessage:  "{\"reason\":\"field password contain latin characters and numbers without space only\"}\n",
 		},
 		{
 			name: "status 400 with empty username",
 			request: `{"username":"    ",
               "password":"12414password"}`,
 			expectedResponse: http.StatusBadRequest,
-			expectedMessage:  "field username contain latin characters and numbers without space only",
+			expectedMessage:  "{\"reason\":\"field username contain latin characters and numbers without space only\"}\n",
 		},
 		{
 			name: "status 400 with empty password",
 			request: `{"username":"usernamedadadada",
               "password":"   "}`,
 			expectedResponse: http.StatusBadRequest,
-			expectedMessage:  "field password contain latin characters and numbers without space only",
+			expectedMessage:  "{\"reason\":\"field password contain latin characters and numbers without space only\"}\n",
 		},
 		{
 			name: "status 400 with ASCII symbols, numbers and space inside field username",
 			request: `{"username":"123pa  s@(&!(s",
               "password":"whatsthispassword"}`,
 			expectedResponse: http.StatusBadRequest,
-			expectedMessage:  "field username contain latin characters and numbers without space only",
+			expectedMessage:  "{\"reason\":\"field username contain latin characters and numbers without space only\"}\n",
 		},
 		{
 			name: "status 400 with ASCII symbols, numbers and space inside field password",
 			request: `{"username":"whoisusername",
               "password":"123pa  s@(&!(s"}`,
 			expectedResponse: http.StatusBadRequest,
-			expectedMessage:  "field password contain latin characters and numbers without space only",
+			expectedMessage:  "{\"reason\":\"field password contain latin characters and numbers without space only\"}\n",
 		},
 		{
 			name: "status 400 when field username is too short",
 			request: `{"username":"abc",
               "password":"whatsthispassword"}`,
 			expectedResponse: http.StatusBadRequest,
-			expectedMessage:  "field username could not be less than 6 characters",
+			expectedMessage:  "{\"reason\":\"field username could not be less than 6 characters\"}\n",
 		},
 		{
 			name: "status 400 when field password is too short",
 			request: `{"username":"iamnotauser",
               "password":"abc"}`,
 			expectedResponse: http.StatusBadRequest,
-			expectedMessage:  "field password could not be less than 6 characters",
+			expectedMessage:  "{\"reason\":\"field password could not be less than 6 characters\"}\n",
+		},
+		{
+			name: "status 409 when username already exists",
+			request: `{"username":"passwordpass",
+              "password":"asdqweas"}`,
+			expectedResponse: http.StatusConflict,
+			expectedMessage:  "{\"reason\":\"user already exists\"}\n",
+		},
+		{
+			name: "another status 409 when username already exists",
+			request: `{"username":"1234username",
+              "password":"12414password"}`,
+			expectedResponse: http.StatusConflict,
+			expectedMessage:  "{\"reason\":\"user already exists\"}\n",
 		},
 	}
 	handler := createHandler()
 	for _, tc := range tt {
-		rec := httptest.NewRecorder()
-		bodyreq := bytes.NewBuffer([]byte(tc.request))
-		req, _ := http.NewRequest("POST", "/register", bodyreq)
-		handler.handleRegistration(rec, req)
-		assert.Equal(t, rec.Result().StatusCode, tc.expectedResponse, tc.name)
-		var jsonMessage message
-		json.NewDecoder(rec.Body).Decode(&jsonMessage)
-		assert.Equal(t, jsonMessage.Reason, tc.expectedMessage, tc.expectedMessage, "\n", tc.name)
+		t.Run(tc.name, func(t *testing.T) {
+			rec := httptest.NewRecorder()
+			bodyreq := bytes.NewBuffer([]byte(tc.request))
+			req := httptest.NewRequest("POST", "/register", bodyreq) // todo user httptest
+			handler.handleRegistration(rec, req)
+			res := rec.Result()
+			defer res.Body.Close()
+			responseBody, err := ioutil.ReadAll(res.Body)
+			if err != nil {
+				t.Error(err)
+			}
+			assert.Equal(t, res.StatusCode, tc.expectedResponse, tc.name)
+			assert.Equal(t, string(responseBody), tc.expectedMessage, "unexpected body")
+		})
 	}
 }
 
-func TestLoginAndNextEkadashiFunc(t *testing.T) {
+func TestLogin(t *testing.T) {
 	tt := []struct {
 		name             string
 		requst           string
@@ -172,57 +193,59 @@ func TestLoginAndNextEkadashiFunc(t *testing.T) {
 			requst: `{"username":"1234username",
               "password":"12414password"`,
 			expectedResponse: http.StatusBadRequest,
-			expectedMessage:  "can not decode the request: unexpected EOF",
+			expectedMessage:  "{\"reason\":\"can not decode the request: unexpected EOF\"}\n",
 		},
 		{
 			name: "another status 400 with incorrect body request",
 			requst: `"1234username",
               "password":"12414password"`,
 			expectedResponse: http.StatusBadRequest,
-			expectedMessage:  "can not decode the request: json: cannot unmarshal string into Go value of type server.loginRequest",
+			expectedMessage:  "{\"reason\":\"can not decode the request: json: cannot unmarshal string into Go value of type server.loginRequest\"}\n",
 		},
 		{
 			name: "status 401 when user doesn't exist",
 			requst: `{"username":"newusernameus",
               "password":"newpasswordus"}`,
 			expectedResponse: http.StatusUnauthorized,
-			expectedMessage:  "incorrect username or password: mongo: no documents in result",
+			expectedMessage:  "{\"reason\":\"incorrect username or password: mongo: no documents in result\"}\n",
 		},
 		{
 			name: "another status 401 when user doesn't exist ",
 			requst: `{"username":"newusernameus",
               "password":"userpass"}`,
 			expectedResponse: http.StatusUnauthorized,
-			expectedMessage:  "incorrect username or password: mongo: no documents in result",
+			expectedMessage:  "{\"reason\":\"incorrect username or password: mongo: no documents in result\"}\n",
 		},
 		{
 			name: "status 401 when field password is incorrect",
 			requst: `{"username":"passwordpass",
-              "password":"qwihoaslk"}`,
+				"password":"qwihoaslk"}`,
 			expectedResponse: http.StatusUnauthorized,
-			expectedMessage:  "incorrect username or password: crypto/bcrypt: hashedPassword is not the hash of the given password",
+			expectedMessage:  "{\"reason\":\"incorrect username or password: crypto/bcrypt: hashedPassword is not the hash of the given password\"}\n",
 		},
 		{
 			name: "another status 401 when field password is incorrect",
 			requst: `{"username":"1234username",
               "password":"asda21"}`,
 			expectedResponse: http.StatusUnauthorized,
-			expectedMessage:  "incorrect username or password: crypto/bcrypt: hashedPassword is not the hash of the given password",
+			expectedMessage:  "{\"reason\":\"incorrect username or password: crypto/bcrypt: hashedPassword is not the hash of the given password\"}\n",
 		},
 	}
 	handler := createHandler()
 	for _, tc := range tt {
-		rec := httptest.NewRecorder()
-		bodyreq := bytes.NewBuffer([]byte(tc.requst))
-		req, _ := http.NewRequest("POST", "/register", bodyreq)
-		handler.handleLogin(rec, req)
-		assert.Equal(t, rec.Result().StatusCode, tc.expectedResponse, tc.name)
-		var jsonMessage message
-		json.NewDecoder(rec.Body).Decode(&jsonMessage)
-		assert.Equal(t, jsonMessage.Reason, tc.expectedMessage, tc.expectedMessage, "\n", tc.name)
-
-		if rec.Result().StatusCode == 200 {
-			handler.nextEkadashiHandler(rec, req)
-		} // this seems do be undone for a moment. Need to ask
+		t.Run(tc.name, func(t *testing.T) {
+			rec := httptest.NewRecorder()
+			bodyreq := bytes.NewBuffer([]byte(tc.requst))
+			req := httptest.NewRequest("POST", "/register", bodyreq)
+			handler.handleLogin(rec, req)
+			res := rec.Result()
+			defer res.Body.Close()
+			responseBody, err := ioutil.ReadAll(res.Body)
+			if err != nil {
+				t.Error(err)
+			}
+			assert.Equal(t, res.StatusCode, tc.expectedResponse, tc.name)
+			assert.Equal(t, string(responseBody), tc.expectedMessage, "unexpected body")
+		})
 	}
 }
